8790bbd938fe59c8cb4d5cdc77d8b00c4dbfb8c9
nanjizal <nanjizal@users.noreply.github.com>
2026-06-06 12:26:45 +0100

    Enhance HxbNodeKind with descriptive comments
    Added comments to clarify the purpose of each HxbNodeKind.

--- creeling/HxbNodeKind.hx
+++ creeling/HxbNodeKind.hx
@@ -1,10 +1,10 @@
 package creeling; 
 
 enum abstract HxbNodeKind( String ) from String to String {
-  var NodeAlloc = "ALLOC";
-  var NodeVarAccess = "VAR_ACCESS";
-  var NodeCall = "METHOD_CALL";
-  var NodeIfElse = "IF_ELSE";
-  var NodeBlock = "BLOCK";
-  var NodeFieldStore = "FIELD_STORE";
+  var NodeAlloc      = "ALLOC";        // Track object heap allocations.
+  var NodeVarAccess  = "VAR_ACCESS";   // Capture lifetime boundaries for references.
+  var NodeCall       = "METHOD_CALL";  // Identify funcition entries for specialized cloning.
+  var NodeIfElse     = "IF_ELSE";      // Process divergent execution flow.
+  var NodeBlock      = "BLOCK";        // 
+  var NodeFieldStore = "FIELD_STORE";  // Capture escape states when objects move to class properties.
 }
