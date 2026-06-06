2423885921f1fa33128cb49619d0ba0da8778e3c
nanjizal <nanjizal@users.noreply.github.com>
2026-06-06 12:54:09 +0100

    Add FlowContext class with cloning functionality

---
+++ creeling/FlowContext.hx
@@ -0,0 +1,16 @@
+package creeling;
+
+import haxe.ds.Map;
+
+class FlowContext {
+  public var variables = new Map<Int, VariableTrack>();
+  public function new(){}
+  public function clone(): FlowContext {
+      var newCtx = new FlowContext();
+      for( k in variables.keys() ){
+          var v = variables.get( k );
+          newCtx.variables.set( k, { id: v.id, v.type, state: v.state } );
+      }
+      return newCtx;
+  }
+}
